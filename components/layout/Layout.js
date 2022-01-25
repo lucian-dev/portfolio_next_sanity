10b12de520b8f4ba42144dc13045e0940cf070ab
lucian-dev <lucian.dev13@gmail.com>
2022-01-25 09:58:22 +0200

    #meta

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,4 +1,3 @@
-import Head from 'next/head';
 import Image from 'next/image';
 import { useTheme } from 'next-themes';
 import Navbar from '@components/navbar/Navbar';
@@ -7,16 +6,29 @@ import styles from './Layout.module.scss';
 
 const Layout = ({ children }) => {
   const { setTheme } = useTheme('dark');
+
   return (
     <>
       <Navbar />
       <main className={styles.mainWrapper}>
         <div className="switch-mode">
           <span className="switch-mode__light">
-            <Image src="/theme_mode.svg" width={22} height={22} onClick={() => setTheme('light')} alt="Theme Mode" />
+            <Image
+              src="/theme_mode.svg"
+              width={22}
+              height={22}
+              onClick={() => setTheme('light')}
+              alt="Theme Mode"
+            />
           </span>
           <span className="switch-mode__dark">
-            <Image src="/theme_mode.svg" width={22} height={22} onClick={() => setTheme('dark')} alt="Theme Mode" />
+            <Image
+              src="/theme_mode.svg"
+              width={22}
+              height={22}
+              onClick={() => setTheme('dark')}
+              alt="Theme Mode"
+            />
           </span>
         </div>
         {children}
