38814705ccb2051be4b8164d0c7898b4da6721f2
lucian-dev <lucian.dev13@gmail.com>
2022-02-20 08:57:23 +0200

    #fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,34 +1,64 @@
+import Script from 'next/script';
 import { useEffect } from 'react';
 import Head from 'next/head';
-import Router from 'next/router';
+import { useRouter } from 'next/router';
+import * as gtag from '../utils/GoogleAnalytics';
 import { ThemeProvider } from 'next-themes';
 import NProgress from 'nprogress';
-import GoogleAnalytics from '@components/GoogleAnalytics';
 import Layout from '@components/layout/Layout';
 import '@styles/globals.scss';
 import '@styles/nprogress.scss';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
   useEffect(() => {
     const start = () => NProgress.start();
     const end = () => NProgress.done();
     NProgress.configure({ easing: 'ease-in-out', speed: 750 });
 
-    Router.events.on('routeChangeStart', start);
-    Router.events.on('routeChangeComplete', end);
-    Router.events.on('routeChangeError', end);
+    router.events.on('routeChangeStart', start);
+    router.events.on('routeChangeComplete', end);
+    router.events.on('routeChangeError', end);
 
     return () => {
-      Router.events.off('routeChangeStart', start);
-      Router.events.off('routeChangeComplete', end);
-      Router.events.off('routeChangeError', end);
+      router.events.off('routeChangeStart', start);
+      router.events.off('routeChangeComplete', end);
+      router.events.off('routeChangeError', end);
     };
-  }, []);
+  }, [router.events]);
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <Head>
         <title>Lucian-DEV -- Freelance Front-end Developer</title>
-        {process.env.NODE_ENV === 'production' && <GoogleAnalytics />}
       </Head>
       <ThemeProvider storageKey="theme" defaultTheme="dark" enableColorScheme={false}>
         <Layout>
