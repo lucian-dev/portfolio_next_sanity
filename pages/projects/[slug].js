63db36acc033f9698a1beee22eb1ef602a38ab10
lucian-dev <lucian.dev13@gmail.com>
2022-03-08 11:17:42 +0200

    #sm

--- pages/projects/[slug].js
+++ pages/projects/[slug].js
@@ -34,7 +34,7 @@ export const getStaticProps = async ({ params: { slug } }) => {
   const project = await client.fetch(projectDetail(`${slug}`));
   const testimonials = await client.fetch(allTestimonials);
 
-  return { props: { project, testimonials }, revalidate: 30 };
+  return { props: { project, testimonials } };
 };
 
 export const getStaticPaths = async () => {
--- pages/projects/index.js
+++ pages/projects/index.js
@@ -38,6 +38,5 @@ export const getStaticProps = async () => {
 
   return {
     props: { projects, testimonials },
-    revalidate: 30,
   };
 };
